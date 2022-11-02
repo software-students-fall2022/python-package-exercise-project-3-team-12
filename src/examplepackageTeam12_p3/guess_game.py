34919b9a3da869eea2b1da96226cad8e64d2dbfa
jkolib <kolibachukjohn@gmail.com>
2022-11-02 16:27:58 -0400

    added sample animal

--- src/examplepackageTeam12_p3/guess_game.py
+++ src/examplepackageTeam12_p3/guess_game.py
@@ -1,4 +1,10 @@
-
+sample = {'name': 'lion', 'turns': 20, 'interactions': {
+    'poke': ['it roars and bites your hand off', 10],
+    'look': ['its yellow fur glistens gracefully in the sun', 1],
+    'yell': ['it glances over at you and yawns, bearing its four large canines', 2],
+    'look around': ['surrounding you are beautiful African plains', 1],
+    'pet': ['its large mane feels good through your fingers', 3]
+    }, 'letter_match': 1}
 
 
 def import_file(fpath):
@@ -11,7 +17,6 @@ def interact(action):
     takes action as string and prints result of action.
 	accesses a dictionary of tuples where tuples store result of action and turn/healthpoint loss.
     '''
-
 
 
 def letter_match(letter):
