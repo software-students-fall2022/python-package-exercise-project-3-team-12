282f940a8bcf3c8c42fb24560b751532e4891bd7
Yvonne511 <yw4142@nyu.edu>
2022-11-07 14:45:08 -0500

    Import Update

--- src/examplepackageTeam12_p3/__main__.py
+++ src/examplepackageTeam12_p3/__main__.py
@@ -2,12 +2,11 @@ import wisdom as wisdom
 import guess_game as game
 
 ##src/examplepackageTeam12_p3/imported_file.txt
-path = 'imported_file.txt'
+##path = 'imported_file.txt'
 
 def main():
-  ##line = wisdom.get()
-  ##print(line)
-  game.import_file(path)
+  line = wisdom.get()
+  print(line)
 
 if __name__ == '__main__':
     main()
