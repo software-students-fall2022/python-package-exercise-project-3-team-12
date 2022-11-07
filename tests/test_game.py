f4804de9c2666028969fcb1febfdd8feab09e514
TheAlexanderChen <alexchen7889@gmail.com>
2022-11-07 11:00:58 -0500

    Began working on tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,5 +1,5 @@
 import pytest
-from examplepackageTeam12p3 import guess_game
+from examplepackageTeam12p3 import guess_game as game
 
 class Tests:
 
@@ -14,6 +14,18 @@ class Tests:
 
         # place with any teardown you want to do after any test function that uses this fixture has completed
     
+    # def load_fixture(self):
+    #     """
+    #     fixture for loading in data
+    #     """
+    #     yield
+
+    # def load_fixture_false(self):
+    #     """
+    #     fixture for loading in data incorrectly
+    #     """
+    #     yield
+
     def test_sanity_check(self, example_fixture):
         """
         Test debugging... making sure that we can run a simple test that always passes.
@@ -28,33 +40,68 @@ class Tests:
         """
         Test if guess correctly identifies that the strings match
         """
+        assert game.guess("lion") == True
+        assert game.guess("LION") == True
+        assert game.guess("tiger") == False
 
     def test_guess_lower(self):
         """
-        Test if guess properly lowers the turns after a failed guess and resets the game after a successfull guess
+        Test if guess properly lowers the turns after a failed guess
         """
+        before = game.animal['turns']
+        game.guess("tiger")
+        assert before > game.animal['turns']
 
     def test_interact(self):
         """
-        Test if interact properly displays its string
+        Test if interact properly returns its string and lowers the score
         """
-    
-    def test_interact_lower(self):
-        """
-        Test if interact properly lowers the turns after an interact
-        """
+        for key in game.animal['interactions']:
+            before = game.animal['interactions'][key][1] 
+            assert game.animal['interactions'][key][0] == game.interact(key)
+            assert before > game.animal['interactions'][key][1] 
 
-    def test_import(self):
-        """
-        Test if import properly imports into the format for animal
-        """
-    
-    def test_letter(self):
-        """
-        Test if letter_match properly checks if the letter is in the animal name
-        """
+    # def test_interact_repeat(self):
+    #     """
+    #     Test if interact does not repeat hints
+    #     """
 
-    def test_letter_lower(self):
-        """
-        Test if letter_match properly lowers the turn count
-        """
+    # def test_import(self):
+    #     """
+    #     Test if import properly imports into the format for animal
+    #     """
+
+    # def test_import_incorrect(self):
+    #     """
+    #     Test if import properly handles incorrect import types
+    #     """
+
+    # def test_letter(self):
+    #     """
+    #     Test if letter_match properly checks if the letter is in the animal name
+    #     """
+
+    # def test_letter_lower(self):
+    #     """
+    #     Test if letter_match properly lowers the turn count
+    #     """
+
+    # def test_letter_repeat(self):
+    #     """
+    #     Test if letter_match does not repeat hints
+    #     """
+
+    # def test_letter_incorrect(self):
+    #     """
+    #     Test if letter_match handles incorrect input like too many letters
+    #     """    
+
+    # def test_play_win(self):
+    #     """
+    #     Test if play returns the correct value if you guess correctly
+    #     """
+
+    # def test_play_lose(self):
+    #     """
+    #     Test if play returns the correct value if you guess incorrectly
+    #     """
